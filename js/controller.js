74951eebfa07a004e4e8fd1fcb64f321b3557fe0
Miles Florence <milesflo15@gmail.com>
2016-02-04 14:57:59 -0800

    Added some noice features

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,7 @@
 app.controller("Catalogue", function($scope, $rootScope, $routeParams, $http, $location) {
 	$scope.message = "Hello!";
+	$http.get("./../teas.json").then(function(response) {
+		console.log(response);
+		$scope.items = response.data;
+	})
 })
