4a61dc842001666b2e0a892c3b4872b932a7106c
Miles Florence <milesflo15@gmail.com>
2016-02-04 15:34:11 -0800

    More noiceness

--- js/controller.js
+++ js/controller.js
@@ -3,5 +3,12 @@ app.controller("Catalogue", function($scope, $rootScope, $routeParams, $http, $l
 	$http.get("./../teas.json").then(function(response) {
 		console.log(response);
 		$scope.items = response.data;
+		// $scope.orderBy = "price";
+
+		for (var i = 0; i < $scope.items.length; i++) {
+			if ($scope.items[i].inStock == true) {
+				$scope.items[i].inStock = "Yes";
+			}
+		}
 	})
 })
