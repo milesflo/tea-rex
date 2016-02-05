3afec6695b886039791776791ec500f8a0f12e1f
Eric Hodges <ehodges2004@gmail.com>
2016-02-05 15:11:54 -0800

    Added link to cart

--- js/services.js
+++ js/services.js
@@ -5,7 +5,7 @@ app.factory('noiceCart', function() {
 	noice.cart = [];
 
 	noice.addCart = function(obj, quant) {
-		quantity = parseInt(quant);
+		quantity = parseInt(quant) || 1;
 		for (i= 0; i < noice.cart.length; i++) {
 			if (noice.cart[i].name == obj.name) {
 				noice.cart[i].quantity += quantity;
@@ -18,7 +18,7 @@ app.factory('noiceCart', function() {
 	};
 
 
-	noice.deleteCart = function(obj) {
+	noice.deleteItem = function(obj) {
 		var index = $scope.cart.indexOf(obj);
 		$scope.cart.splice(index, 1);
 		console.log(noice.cart);
